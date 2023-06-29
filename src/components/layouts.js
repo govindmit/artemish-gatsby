65e37b3ae4c090d6be2d05e7133dfd9f32eea4af
shubham jaiswal <sj2585097@gmail.com>
2023-06-29 11:09:39 +0530

    working on layouts

---
+++ src/components/layouts.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Header from "./common/header";
+import Footer from "./common/footer";
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
