9b4e7f462c7200a891b5e4c03ffc81129f9fb0e8
shubham jaiswal <sj2585097@gmail.com>
2023-07-05 13:20:40 +0530

    create navbarmenu

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,46 +1,17 @@
 import type { GatsbyConfig } from "gatsby";
-import axios from "axios";
-
-// let token: string;
-
-//   const qs = require('qs');
-//   let data:any = qs.stringify({
-//     'username': 'vipulkmango',
-//     'password': 'vipulkmango'
-//   });
-
-//   axios.request({
-//     method: "post",
-//     maxBodyLength: Infinity,
-//     url: "https://artemish.mangoitsol.com/wp-json/jwt-auth/v1/token",
-//     headers: {
-//       'Content-Type': 'application/x-www-form-urlencoded',
-//     },
-//     data : data
-//   })
-//   .then((response) => {
-//     console.log(JSON.stringify(response.data));
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Artemish`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
-  // If you use VSCode you can also use the GraphQL plugin
-  // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
+    "gatsby-plugin-postcss",
     {
       resolve: "gatsby-source-graphql",
       options: {
         typeName: "WPGraphQL",
         fieldName: "wpcontent",
-        // GraphQL endpoint, relative to your WordPress home URL.
         url:
           process.env.WPGRAPHQL_URL ||
           `https://artemish.mangoitsol.com/graphql`,
@@ -49,7 +20,6 @@ const config: GatsbyConfig = {
         },
       },
     },
-
     {
       resolve: "gatsby-source-wordpress",
       options: {
@@ -63,22 +33,6 @@ const config: GatsbyConfig = {
             exclude: true,
           },
         },
-        // baseUrl: "artemish.mangoitsol.com",
-        // protocol: "https",
-        // restApiRoutePrefix: "wp-json",
-        // perPage: 100,
-        // jwt_user: "vipulkmango",
-        // jwt_pass: "vipulkmango",
-        // jwt_base_path: "/jwt-auth/v1/token",
-        // includedRoutes: [
-        //   "**/categories",
-        //   "**/posts",
-        //   "**/pages",
-        //   "**/products",
-        //   "**/tags",
-        //   "**/taxonomies",
-        //   "**/users",
-        // ]
       },
     },
     "gatsby-plugin-image",
