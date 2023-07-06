ca1ecc115d5d50a1888ede9389a92648cd5b3506
shubham jaiswal <sj2585097@gmail.com>
2023-07-06 12:00:37 +0530

    push code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,23 @@
 import * as React from "react"
 import Layout from "../components/layouts.js";
 import SEO from "../components/common/seo.js";
-import type { HeadFC, PageProps } from "gatsby";
-
+import VideoSec from "../components/AtrimeshHomeCmp/videoSec.js";
+import { type HeadFC, type PageProps } from "gatsby";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 const IndexPage: React.FC<PageProps> = () => {
   const seoTitle = "Artemis Defense Institute";
+  const client = new ApolloClient({
+    uri: 'https://artemish.mangoitsol.com/wp-json',
+    cache: new InMemoryCache(),
+  });
   return (
     <>
-      <Layout>
-        <SEO title={seoTitle} />
-        <div className="index-page">
-          hii
-        </div>
-      </Layout>
+      <ApolloProvider client={client}>
+        <Layout>
+          <SEO title={seoTitle} />
+          <VideoSec />
+        </Layout>
+      </ApolloProvider>
     </>
   )
 }
